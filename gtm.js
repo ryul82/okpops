e60c9ddfd9bd433180b207c53b45fa15513b55f3
ryul82 <60336361+ryul82@users.noreply.github.com>
2020-02-21 17:16:16 +0900

    Update gtm.js

--- gtm.js
+++ gtm.js
@@ -1,6 +1,2 @@
-$(document).ready(function(){
-
-  document.getElementById("gtm").innerHTML='<object type="text/html" data="gtm.html"></object>';
-
-})
+$(document).ready(function(){document.getElementById("gtm").innerHTML='<object type="text/html" data="gtm.html"></object>';})
 
