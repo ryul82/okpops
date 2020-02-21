e3998158ba8c1f6494e0d96b096a0a61e2c0a290
ryul82 <60336361+ryul82@users.noreply.github.com>
2020-02-21 17:39:57 +0900

    new

--- gtm_1.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  document.getElementById("gtm_1").innerHTML='<object type="text/html" data="gtm_1.html"></object>';
-})
-
--- gtm_2.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  document.getElementById("gtm_2").innerHTML='<object type="text/html" data="gtm_2.html"></object>';
-})
-
