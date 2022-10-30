515fa7750d4f408c6de1c1b375a3dc4bd7282f21
ryul82 <60336361+ryul82@users.noreply.github.com>
2022-10-30 22:59:01 +0900

    3

--- includeHTML.js
+++ includeHTML.js
@@ -1,35 +1,20 @@
-function includeHTML(callback) {
-  var z, i, elmnt, file, xhr;
-  /*loop through a collection of all HTML elements:*/
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
+	elmnt = z[i];
     file = elmnt.getAttribute("include-html");
-    //console.log(file);
     if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /*remove the attribute, and call this function once more:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          elmnt.innerHTML = this.responseText;
           elmnt.removeAttribute("include-html");
-          includeHTML(callback);
-        }
-      };
-      xhr.open("GET", file, true);
-      xhr.send();
-      /*exit the function:*/
+          includeHTML();        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
       return;
     }
   }
-  setTimeout(function() {
-    callback();
-  }, 0);
 }
