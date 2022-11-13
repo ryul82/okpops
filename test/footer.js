34edbc09c89f1d1eee1e7504a34b508644cec70e
labs <labs.dwcts@gmail.com>
2022-11-13 18:01:59 +0900

    update

--- test/footer.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-  document.getElementById("footer").innerHTML =
-    '<object type="text/html" data="./footer.html"></object>';
-});
--- test/include.js
+++ test/include.js
@@ -1,36 +1,18 @@
-function includeHTML() {
-  let z, elmnt, file, xhttp;
+<script src="http://code.jquery.com/jquery.min.js"></script>
+<script>
+	$(function(){
+		includeLayout();
+	});
 
-  z = document.getElementsByTagName("*");
-
-  for (let i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    file = elmnt.getAttribute("data-include");
-
-    if (file) {
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-          if (this.status == 200) {
-            elmnt.innerHTML = this.responseText;
-          }
-          if (this.status == 404) {
-            elmnt.innerHTML = "Page not found.";
-          }
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("data-include");
-          includeHTML();
-        } //if
-      }; //onreadystatechange
-
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      return;
-    } //if - file
-  } //for
-} //includeHTML
-
-/* ✨ 실행 */
-window.addEventListener("DOMContentLoaded", () => {
-  includeHTML();
-});
+	function includeLayout(){
+		var includeArea = $('[data-include]');
+		var self, url;
+		$.each(includeArea, function() {
+			self = $(this);
+			url = self.data("include");
+			self.load(url, function() {
+				self.removeAttr("data-include");
+			});
+		});
+	}
+</script>
